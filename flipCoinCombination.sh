9a6e6083a6ee212dc20fd31393d0ebd697700c5d
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-07-27 23:53:15 +0530

    Doublet Combination with %

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,25 +2,37 @@
 
 HEAD=0
 TAIL=0
-HEADPER=0
-TAILPER0=0
+
+HH=0
+HT=0
+TH=0
+TT=0
 
 for (( i=1; i<=100; i++ ))
 do
-        random1=$((RANDOM%2))
-        declare -A coin
-        coin[$i]=$random1
-done
+        ran=$((RANDOM%4))
+        if (( $ran == 0 ))
+    then
+        HH=$(($HH+1))
+        #echo "HH"
 
-for i in ${coin[@]}
-do
-        if (( $i == 1 ))
-        then
-        HEADPER=$(($HEADPER+1))
-        else
-        TAILPER=$(($TAILPER+1))
+
+    elif (( $ran == 1 ))
+    then
+        HT=$(($HT+1))
+        #echo "HT"
+
+
+    elif (( $ran == 2 ))
+    then
+        TH=$(($TH+1))
+        #echo "TH"
+
+
+    elif (( $ran == 3 ))
+    then
+        TT=$(($TT+1))
+        #echo "TT"
         fi
 done
-        echo "HEAD (%) :" $HEADPER"%"
-        tailpercentage=$(( ((${#coin[@]}))-$HEADPER ))
-        echo "TAIL (%) :" $tailpercentage"%"
+echo "HH :"$HH"%" "HT :"$HT"%" "TH :"$TH"%" "TT :"$TT"%"
